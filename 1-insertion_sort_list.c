bf82accfe89491e81b0eed56d3d2c1e64358fb0a
Abu Jehoiada <abujehoiada@gmail.com>
2024-04-26 03:39:53 -0700

    Fixed betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -19,7 +19,7 @@ void insertion_sort_list(listint_t **list)
 		tmp = curr->next;
 		tras = curr;
 
-		while(tras->prev != NULL && (tras->n < tras->prev->n))
+		while (tras->prev != NULL && (tras->n < tras->prev->n))
 		{
 			tras->prev->next = tras->next;
 			if (tras->next != NULL)
@@ -36,45 +36,4 @@ void insertion_sort_list(listint_t **list)
 		}
 		curr = tmp;
 	}
-
-			      
-/*	listint_t *sorted, *curr, *nxt, *tmp, *new;
-
-	sorted = NULL;
-	curr = *list;
-
-	while (curr != NULL)
-	{
-		nxt = curr->next;
-		if (sorted == NULL || sorted->n > curr->n)
-		{
-			curr->next = sorted;
-			sorted = curr;
-			print_list(sorted);
-		}
-		else
-		{
-			new = sorted;
-			while (new->next != NULL && new->next->n <= curr->n)
-				new = new->next;
-			if (new->next == NULL)
-			{
-				new->next = curr;
-				curr->prev = new;
-				curr->next = NULL;
-			}
-			else
-			{
-				tmp = new->next;
-				new->next = curr;
-				curr->prev = new;
-				curr->next = tmp;
-				tmp->prev = curr;
-			}
-			print_list(sorted);
-		}
-		curr = nxt;
-	}
-	*list = sorted;
-*/
 }
